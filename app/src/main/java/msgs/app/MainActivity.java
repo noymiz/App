a965de2710c02cc549866ce04b91525d974fcc6a
Noy <mizrahi.noy@gmail.com>
2016-06-11 14:35:35 +0300

    what we have done so far

--- app/src/main/java/msgs/app/MainActivity.java
+++ app/src/main/java/msgs/app/MainActivity.java
@@ -1,52 +1,69 @@
 package msgs.app;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView message;
+    private String msg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        message = (TextView) findViewById(R.id.textView);
+        changeMessage();
+    }
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+    private void changeMessage() {
+        Thread t = new Thread(new Runnable(){
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void run() {
+                ArrayList<String> msgs = new ArrayList<>();
+                msgs.add("Text");
+                msgs.add("Chat");
+                msgs.add("Message");
+                msgs.add("Fun");
+
+                for (int i = 0; i<4; i++) {
+                    msg = msgs.get(i);
+                    runOnUiThread(new Runnable() {
+
+                        @Override
+                        public void run() {
+                            message.setText(msg);
+                        }
+                    });
+
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+                SharedPreferences.Editor editor = settings.edit();
+                Intent intent;
+
+                if (settings.getString("username", "User not found").equals("User not found")) {
+                    intent = new Intent(MainActivity.this, LoginActivity.class);
+                }
+                else{
+                    intent = new Intent(MainActivity.this, MenuActivity.class);
+                }
+                startActivity(intent);
             }
         });
+        t.start();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
